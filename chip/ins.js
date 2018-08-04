31253c8f37a7ac478f3d1097cabe95b87ea0ac27
jimmy <1120213119@qq.com>
2018-08-04 10:51:29 +0800

    Site updated: 2018-08-04 10:51:29

--- chip/ins.js
+++ chip/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'http://og1t4des6.bkt.clouddn.com/' + data.link[i] + '.jpg!mini';
-          var src = 'http://og1t4des6.bkt.clouddn.com/' + data.link[i];
+          var minSrc = 'http://pcrd0aeyo.bkt.clouddn.com/' + data.link[i] + '.jpg!mini';
+          var src = 'http://pcrd0aeyo.bkt.clouddn.com/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '.jpg';
